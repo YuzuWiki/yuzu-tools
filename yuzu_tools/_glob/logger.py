55f7caab364a9f3d89ab75e14441985fd0ef696d
lixianjun <lixianjun@touchdata.io>
2022-01-12 16:22:16 +0800

    [Fix] logger | fix logger error

--- yuzu_tools/_glob/logger.py
+++ yuzu_tools/_glob/logger.py
@@ -13,16 +13,31 @@ def setLogger(logger: "__logging.Logger"):
         __logger = logger
 
 
-def getLogger(name: str = Frame_Name) -> "__logging.Logger":
+def _get_logger() -> "__logging.Logger":
+    return __logger
+
+
+def getLogger(name: str = Frame_Name, level: int = __logging.DEBUG) -> "__logging.Logger":
     global __logger
 
     if not __logger:
-        __logger = __logging.getLogger("yuzu_tools")
+
+        # # handler 设置
+        handler = __logging.StreamHandler()
+        handler.setLevel(level)
+        handler.setFormatter(
+            __logging.Formatter('| %(asctime)s.%(msecs)04d | %(name)-10s | %(levelname)-6s:  %(message)s',
+                                datefmt="%Y-%m-%d %H:%M:%S"))
+
+        # 配置默认的logger
+        __logger = __logging.Logger(name)
+        __logger.addHandler(handler)
+
 
     class Proxy:
-        def __getattr__(self, item):
+        def __getattr__(self, level):
             global __logger
-            return getattr(__logger, item)
+            return getattr(_get_logger(), level)
 
     return Proxy() # noqa
 
